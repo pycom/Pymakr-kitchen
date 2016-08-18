c67b17b9d90e6790f0d2a7dd5e70d3973b1c1e45
Your Name <you@example.com>
2016-08-18 20:03:20 +0200

    linux/: improve kitchen and bump into 1.0.0.b3

--- linux/makepackages.sh
+++ linux/makepackages.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env sh
 
-rm *.deb
-(cd files;dpkg-buildpackage -b)
 
-rm -R pymakr-1.0.0.b1
+
+release_version=$(grep "pymakr (.*)"  build_files/debian/changelog | head -1 | cut -d ' ' -f2 | grep -o "[^()]*")
+short_version=$(echo $release_version | cut -d '-' -f1)
+
+rm *.deb
+(cd build_files;dpkg-buildpackage -b)
+
+rm -R pymakr-$short_version
+
+echo "generating the rpm"
 alien -rkg *.deb
 
-sed pymakr-1.0.0.b1/pymakr-1.0.0.b1-1.spec -e 's|\(Summary:.*\)[\.]$|\1|' | 
+sed pymakr-$short_version/pymakr-$release_version.spec -e 's|\(Summary:.*\)[\.]$|\1|' | 
 	sed 's|Group: .*$|Group: Applications/Editors|' | 
 	sed 's|^.*_rpmdir.*$|%define _rpmdir .|' |
 	sed 's|^.*Converted from a deb package.*$|An editor to work with Pycom products|' |
@@ -15,8 +22,8 @@ sed pymakr-1.0.0.b1/pymakr-1.0.0.b1-1.spec -e 's|\(Summary:.*\)[\.]$|\1|' |
 	sed "/Group:.*$/a URL: http://www.pycom.io/solutions/pymakr/" | 
 	sed '/^Distribution:/d' |
 	sed '/%dir "\/"/d' |
-	sed '/%dir "\/usr\/bin\/"/d' > pymakr-1.0.0.b1/pymakr-1.0.0.b1-1.spec.out
+	sed '/%dir "\/usr\/bin\/"/d' > pymakr-$short_version/pymakr-$release_version.spec.out
 
-mv pymakr-1.0.0.b1/pymakr-1.0.0.b1-1.spec.out pymakr-1.0.0.b1/pymakr-1.0.0.b1-1.spec
+mv pymakr-$short_version/pymakr-$release_version.spec.out pymakr-$short_version/pymakr-$release_version.spec
 
-rpmbuild -v -bb pymakr-1.0.0.b1/pymakr-1.0.0.b1-1.spec --buildroot=$PWD/pymakr-1.0.0.b1
+rpmbuild -v -bb pymakr-$short_version/pymakr-$release_version.spec --buildroot=$PWD/pymakr-$short_version
